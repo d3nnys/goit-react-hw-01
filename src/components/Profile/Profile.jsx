f85875196364116a43a2013c10fab183482b93a4
Denys Puzikov <denys.puzikov@gmail.com>
2024-03-30 13:08:51 +0100

    first try

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,35 @@
+import css from './Profile.module.css';
+
+export default function Profile({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, likes, views },
+}) {
+  return (
+    <div className={css.cardWrapper}>
+      <div className={css.card}>
+        <img className={css.cardImg} src={avatar} alt={tag} />
+        <p className={css.userName}>{username}</p>
+        <p className={css.text}>{tag}</p>
+        <p className={css.text}>{location}</p>
+      </div>
+
+      <ul className={css.cardList}>
+        <li className={css.cardListItem}>
+          <span className={css.itemText}>Followers</span>
+          <span className={css.itemTextCount}>{followers}</span>
+        </li>
+        <li className={css.cardListItem}>
+          <span className={css.itemText}>Views</span>
+          <span className={css.itemTextCount}>{views}</span>
+        </li>
+        <li className={css.cardListItem}>
+          <span className={css.itemText}>Likes</span>
+          <span className={css.itemTextCount}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
